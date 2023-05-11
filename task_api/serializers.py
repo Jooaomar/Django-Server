c6d8ceab68294dc1e8191f3b917bb387df4e0d9b
João Marcelo <jmrcl91@gmail.com>
2023-05-11 11:17:30 -0300

    Atuaçlizando

--- task_api/serializers.py
+++ task_api/serializers.py
@@ -10,22 +10,22 @@ CHOICE_SITUACAO =  [('nova','nova'),
 
 class TasksSerializer(serializers.ModelSerializer):
 
-    descricao = serializers.CharField()
-    situacao = serializers.ChoiceField(choices=CHOICE_SITUACAO)
-    responsavel = serializers.CharField(max_length=250, default='')
+    # descricao = serializers.CharField()
+    # situacao = serializers.ChoiceField(choices=CHOICE_SITUACAO)
+    # responsavel = serializers.CharField(max_length=250, default='')
 
-    def validate_descricao(self, value):
-        return value.capitalize()
+    # def validate_descricao(self, value):
+    #     return value.capitalize()
     
-    def validate_situacao(self, value):
-        return value.capitalize()
+    # def validate_situacao(self, value):
+    #     return value.capitalize()
     
-    def validate_responsavel(self, value):
-        return value.capitalize()
+    # def validate_responsavel(self, value):
+    #     return value.capitalize()
 
     class Meta:
         
         model = Task
-        fields = ['id', 'responsavel', 'descricao', 'nivel', 'situacao', 'prioridade']
+        fields = ['id', 'created','responsavel', 'descricao', 'nivel', 'situacao', 'prioridade']
 
 
