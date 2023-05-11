9974c443de0b15e242b85ea35254929539570d8a
icaro moroni <icaromorone@gmail.com>
2023-05-10 21:09:03 -0300

    adicionado a url para query

--- task_api/urls.py
+++ task_api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ListarCriarTask, DetalheAtualizarRemoverTask, ListStituacaoNivelPrioridade
+from .views import ListarCriarTask, DetalheAtualizarRemoverTask, ListSituacaoNivelPrioridade
 
 urlpatterns = [
     path('tarefas/', ListarCriarTask.as_view(), name= 'listar-criar-tarefa'),
     path('tarefas/<int:pk>', DetalheAtualizarRemoverTask.as_view(), name='detalhar-atualizar-remover'),
-    path('tarefas/search/', ListStituacaoNivelPrioridade.as_view(), name='Situação-Nivel-Prioridade'),
+    path('search/', ListSituacaoNivelPrioridade.as_view(), name='Situação-Nivel-Prioridade'),
 ]
